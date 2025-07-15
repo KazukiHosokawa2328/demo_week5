7b0de87bf6cd9ff2047b85d0671d58ddb8bda019
KazukiHosokawa2328 <kh2328workkh@outlook.jp>
2025-07-16 06:14:19 +0900

    [add]css追加
    swiperとスケジュールのCSS修正

--- js/index.js
+++ js/index.js
@@ -1,29 +1,27 @@
-var swiper = new Swiper(".mySwiper", {
-	cssMode: true,
-	spaceBetween: 50,
-	slidesPerView: 1.2,
-	centeredSlides: true,
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-	pagination: {
-		el: ".swiper-pagination",
-		clickable: true,
-	},
-	mousewheel: true,
-	keyboard: true,
-	loop: true,
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1.2,
+  centeredSlides: true,
+  spaceBetween: 20,
+  loop: true,
+  //   loopAdditionalSlides: 2,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
 
 //コンテンツ切り替え
 $(function () {
-	$('.schedule-contents p[class != "schedule-contents-first"]').hide();
-	$(".js-tab-link").click(function (e) {
-		e.preventDefault();
-		$(".schedule-contents p").hide();
-		$($(this).attr("href")).show();
-		$("li.schedule-item").removeClass("selected");
-		$(this).parent().addClass("selected");
-	});
+  $('.schedule-contents p[class != "schedule-contents-first"]').hide();
+  $(".js-tab-link").click(function (e) {
+    e.preventDefault();
+    $(".schedule-contents p").hide();
+    $($(this).attr("href")).show();
+    $("li.schedule-item").removeClass("selected");
+    $(this).parent().addClass("selected");
+  });
 });
