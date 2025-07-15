7494506f1eefb81bae373e39f2ca39c565c8ce4b
kkazki42 <hehuixichuan2@gmail.com>
2025-07-15 11:59:18 +0900

    swiperの幅修正、コンテンツの出しわけ追加

--- js/index.js
+++ js/index.js
@@ -1,15 +1,29 @@
 var swiper = new Swiper(".mySwiper", {
-  cssMode: true,
-  spaceBetween: 50,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  mousewheel: true,
-  keyboard: true,
-  loop: true,
+	cssMode: true,
+	spaceBetween: 50,
+	slidesPerView: 1.2,
+	centeredSlides: true,
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	mousewheel: true,
+	keyboard: true,
+	loop: true,
+});
+
+//コンテンツ切り替え
+$(function () {
+	$('.schedule-contents p[class != "schedule-contents-first"]').hide();
+	$(".js-tab-link").click(function (e) {
+		e.preventDefault();
+		$(".schedule-contents p").hide();
+		$($(this).attr("href")).show();
+		$("li.schedule-item").removeClass("selected");
+		$(this).parent().addClass("selected");
+	});
 });
